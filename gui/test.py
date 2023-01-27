3c84f1353b6527529d259d071154d432fe0130e5
zellaa <zellbaig@gmail.com>
2023-01-27 13:01:35 +0000

    Added a way to save images.

--- gui/test.py
+++ gui/test.py
@@ -12,25 +12,41 @@ class Application(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        fig=plt.figure(figsize=(8,8))
-        ax=fig.add_axes([0.1,0.1,0.8,0.8])
+        fig=plt.figure(figsize=(6,4))
+        ax = fig.add_subplot(111)
+        plt.axis('off')
+#        ax=fig.add_axes([0.1,0.1,0.8,0.8])
         canvas=FigureCanvasTkAgg(fig,master=root)
         canvas.get_tk_widget().grid(row=0,column=1)
+        #canvas.get_tk_widget().pack(side="top", fill="both", expand=1)
+        #canvas.get_tk_widget().pack(side="top", fill="x", expand=0)
+        #canvas._tkcanvas.pack(side="top", fill="x", expand=0)
+
         canvas.draw()
 
-        self.plotbutton=tk.Button(master=root, text="plot", command=lambda: self.plot(canvas,ax))
-        self.plotbutton.grid(row=0,column=0)
-        self.exit_button = tk.Button(root, text="Exit", command=root.destroy)
-        self.exit_button.grid(row=0,column=1)
+        self.plotButton=tk.Button(master=root, text="plot", command=lambda: self.displayImage(canvas,ax,fig))
+        self.saveButton=tk.Button(master=root, text="save", command=lambda: self.saveImage(canvas,fig))
+        self.exitButton = tk.Button(root, text="Exit", command=root.destroy)
 
-    def plot(self,canvas,ax):
+        #self.plotbutton.pack(expand=True)
+        #self.exit_button.pack(expand=True)
+
+        self.plotButton.grid(row=0,column=3)
+        self.saveButton.grid(row=2,column=3)
+        self.exitButton.grid(row=3,column=3)
+
+    def displayImage(self,canvas,ax,fig):
         ax.clear()         # clear axes from previous plot
-        filenamme = tk.filedialog.askopenfilename(initialdir='/GUI', title='Select A File', filetypes=(
-        ('png files', '*.png'), ('jpg files', '*.jpg'), ('all files', '*.*')))
+        filenamme = tk.filedialog.askopenfilename(initialdir='../images/', title='Select A File', filetypes=(
+        ('jpg files', '*.jpg'), ('png files', '*.png'), ('all files', '*.*')))
         if filenamme:
             img_arr = mpimg.imread(filenamme)
             ax.imshow(img_arr)
+            plt.axis('off')
             canvas.draw()
+    def saveImage(self,canvas,fig):
+        fig.savefig('./image.png')
+
 
 
 root=tk.Tk()
