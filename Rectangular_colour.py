12b661a8a45f06478a7add67c10aa780b4e35611
zczlelo <77201111+zczlelo@users.noreply.github.com>
2023-01-31 13:04:14 +0000

    Added clicking feature
    Need some improvements code is kinda clumsy

--- Rectangular_colour.py
+++ Rectangular_colour.py
@@ -15,16 +15,44 @@ img_gray_3d = np.stack((img_gray,)*3,axis=-1)
 plt.imshow(img_gray_3d, cmap='gray')
 plt.show()
 
+grayimg=img_gray_3d
 # Function to have a coloured rectangle on
 def colourbox(istart,iend,jstart,jend):
     newimg=image
     size_y,size_x,null=image.shape
-    grayimg=img_gray_3d
     for i in range(istart,iend):
         for j in range(jstart,jend):
             grayimg[i,j,:]=newimg[i,j,0:3] 
-    return plt.imshow(grayimg,cmap='gray')
+  #  return plt.imshow(grayimg,cmap='gray')
+    return grayimg
+img_gray_3d = np.stack((img_gray,)*3,axis=-1)
 
 #Using the function
-colourbox(150,200,150,200)
+#colourbox(150,200,150,200)
+#plt.imshow(grayimg)
+#plt.show()
+
+#Click to colour in the picture (will try to add more features)
+grayimg=img_gray_3d
+fig = plt.figure()
+gs = GridSpec(2,3)
+ax0 = fig.add_subplot(gs[0:2,0:2])
+img = img_gray_3d
+ax0.imshow(img)
+nx,ny,d = img.shape
+def onclick(event):
+    global x,y
+    if event.inaxes==ax0:
+        x,y = event.xdata,event.ydata
+        x,y = np.round(x).astype(int),np.round(y).astype(int)
+        colourbox(y,y+20,x,x+20)
+        img=grayimg
+        ax0.imshow(img)
+        print("clicked image")
+        print("x,y is ", x,y)
+    else:
+        print("Please click on the image to colourise a section")
+    fig.canvas.draw_idle()
+
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
 plt.show()
